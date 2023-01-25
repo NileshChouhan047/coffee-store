5ccf6590868ff1ded69d4f47a9a6e3a85bcd4197
Nilesh Chouhan <mrnileshchouhan6@gmail.com>
2023-01-25 13:04:58 +0530

    ShopController Completed

--- src/main/java/com/coffeeshop/controller/OrderController.java
+++ src/main/java/com/coffeeshop/controller/OrderController.java
@@ -1,5 +1,11 @@
 package com.coffeeshop.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+	
 
 }
